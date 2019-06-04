331ed2ca18cc534b1a3a269cb42af1e43ba7a3e2
Denis Kibet <kibet1816@gmail.com>
2019-06-04 11:26:29 +0300

    Add quote form ts template

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QuoteComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0, '', '', '', 0, 0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+  onSubmit( ) {
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0, '', '', '', 0, 0);
+  }
   constructor() { }
 
   ngOnInit() {
