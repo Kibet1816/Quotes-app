a5ac58bd20c5950ca8bb28ed3d50e241655f5f02
Denis Kibet <kibet1816@gmail.com>
2019-06-04 11:29:57 +0300

    Include quote component ts template

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
-  selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class QuoteFormComponent implements OnInit {
+export class QuoteComponent implements OnInit {
 
-  newQuote = new Quote(0, '', '', '', 0, 0);
+  @Input()quote: Quote;
 
-  @Output() addQuote = new EventEmitter<Quote>();
-  onSubmit( ) {
-    this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote(0, '', '', '', 0, 0);
+  quotes = [
+    // new Quote(1, 'Loyangapuo', 'Kijana mfupi,amenona,round', 0, 0),
+    // new Quote(2, 'Eminem', 'Keep it real', 0, 0),
+    // new Quote(3, 'Men from Valyria', 'What is dead may never die', 0, 0),
+  ];
+  addAnotherQuote (quote: any) {
+   const quoteLength = this.quotes.length;
+   quote.id = quoteLength + 1;
+   this.quotes.unshift(quote);
   }
+  quoteDelete(isComplete, index) {
+    if (isComplete) {
+      const confirmDelete = confirm('Do you want to proceed ??????');
+      if (confirmDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
