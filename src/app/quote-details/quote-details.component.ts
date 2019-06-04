8809661ec04c2b5433bdf35738b79c453fe6e9aa
Denis Kibet <kibet1816@gmail.com>
2019-06-04 11:38:10 +0300

    Add quote details.ts template

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -7,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input()quote: Quote;
+  @Output()isComplete = new EventEmitter<boolean>();
+
+  upvote = 0;
+  upVote() {
+     this.upvote = this.upvote + 1;
+  }
+  downVote() {
+    if (this.upvote > 0) {
+    this.upvote = this.upvote - 1;
+    } else if (this.upvote = 0) {
+      this.upvote = 0;
+    }
+  }
+  deleteQuote(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
