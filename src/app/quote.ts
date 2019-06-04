507425ee03a06dfa7398bcf9880c0b3f27f9ca70
Denis Kibet <kibet1816@gmail.com>
2019-06-04 11:35:12 +0300

    Include instances in the model class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+  constructor(
+    public id: number,
+    public author: string,
+    public quote: string,
+    public publisher: string,
+    public upvote: number,
+    public downvote: number,
+  ) {}
 }
